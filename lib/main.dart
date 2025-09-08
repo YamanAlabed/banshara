45b51cee8864d36adb75b4ddf00d64ff141aef30
Yaman Alabed~ <yamanalabed16@gmail.com>
2025-09-08 09:21:58 +0200

    init Firebase update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -5,6 +6,9 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  if (kDebugMode) {
+    print('Firebase initialized');
+  }
   runApp(const MainApp());
 }
 
