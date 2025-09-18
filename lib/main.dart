8dcd57a5d24b9d24013ebf0600bff2a6912a3397
Yazan LABBAD <yyzn80462@gmail.com>
2025-09-18 14:29:01 +0300

    tseting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'dart:math' as console;
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+if (kDebugMode) {
+ print('initlization firebase');
+}
   runApp(const HomeScreen());
+
 }
 
 class HomeScreen extends StatelessWidget {
