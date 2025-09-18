c8919e453469d463451bc680d6bf3039066aff80
Yaman Alabed~ <yamanalabed16@gmail.com>
2025-09-18 13:15:00 +0200

    added: packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const HomeScreen());
@@ -14,11 +13,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('yaman World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('yaman World!'))),
     );
   }
 }
